bca96aa2297625ddbe832c07debee619fe51fe70
Casey Richardson <caseybrichardson@icloud.com>
2016-10-06 14:48:28 -0500

    Cleaned up some code, added slack notification

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from bf import BFEvaluator
 
+import xml.sax.saxutils as saxutils
+
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask import current_app
+
+import requests
 
 def get_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -14,6 +19,26 @@ app = get_app()
 '''
     Utils
 '''
+
+def execute_bf_code(code):
+    evaluator = BFEvaluator()
+
+    try:
+        success, output = evaluator.evaluate(code)
+    except Exception:
+        return (False, '')
+    else:
+        return (success, output)
+
+'''
+    Response Utils
+'''
+
+def notify_webhook(hook, text):
+    try:
+        requests.post(hook, json={'text': text})
+    except Exception:
+        print('SLACK_MESSAGE_HOOK not configured, skipping message send')
 
 def make_slack_response(text):
     response = jsonify({'text': text})
@@ -37,16 +62,16 @@ def _make_response(message, data, response_code):
 @app.route('/slack/submit', methods=['POST'])
 def slack_submit():
     try:
-        source = request.json['text']
-        trigger = request.json['trigger_word']
+        source = saxutils.unescape(request.form['text'])
+        trigger = request.form['trigger_word']
         source = source[len(trigger):]
     except KeyError as ke:
         return make_slack_response('Missing source in request.')
     except Exception as e:
         return make_slack_response('Unexpected error while processing request.')
     else:
-        evaluator = BFEvaluator()
-        success, output = evaluator.evaluate(source)
+        success, output = execute_bf_code(source)
+
         if success:
             return make_slack_response('Program successfully submitted! Output: ```{}```'.format(output.strip()))
         else:
@@ -61,9 +86,10 @@ def challenge_submit():
     except Exception as e:
         return make_error('Unexpected error while processing request.', response_code=500)
     else:
-        evaluator = BFEvaluator()
-        success, output = evaluator.evaluate(source)
+        success, output = execute_bf_code(source)
+
         if success:
+            notify_webhook(current_app.config.get('SLACK_MESSAGE_HOOK', ''), 'Entry Success: ```{}``` Source: ```{}```'.format(output, source))
             return make_success('Program successfully submitted!', data={'output': output})
         else:
             return make_error('Program failed to be interpreted. Please check source for errors.')
@@ -73,7 +99,7 @@ def challenge_submit():
 '''
 
 def main():
-    app.run(debug=True)
+    app.run(host=app.config.get('HOST', '0.0.0.0'), debug=app.config.get('DEBUG', True))
 
 if __name__ == '__main__':
     main()
